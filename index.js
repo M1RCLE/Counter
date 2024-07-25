ba717539963aa0add55102c2e826074a111a9699
Michael Shindarev <mshindarev@gmail.com>
2024-07-26 00:24:50 +0200

    have fixed some bugs and now it look ok (like apple one based on style)

--- index.js
+++ index.js
@@ -3,15 +3,33 @@
 const display = document.getElementById("display");
 
 let result = 0;
+let refresh = false;
 
 function appendToDisplay(input) {
-    display.value += input
+    if (input >= '0' && input <= '9' || input == '.') {
+        if (!refresh) {
+            display.value += input;
+        } else {
+            display.value = "";
+            display.value += input;
+            refresh = false;
+        }
+    } else {
+        try {
+            console.log("you are here");
+            display.value = eval(result);
+            refresh = true;
+        } catch (error) {
+            display.value = "Error";
+            console.log(error);
+        }
+    }
     result += input;
 }
 
 function calculate() {
     try {
-        display.value = eval(display.value);
+        display.value = eval(result);
     } catch(eroor) {
         display.value = "Error";
         console.log(error);
@@ -20,4 +38,5 @@ function calculate() {
 
 function clearDisplay() {
     display.value = "";
+    result = "";
 }
